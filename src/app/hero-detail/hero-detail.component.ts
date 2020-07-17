31877561807947a945f64a55a0afaa7073b1be2a
Felipe Romero <feliperomero3@outlook.com>
2020-07-16 21:00:00 -0500

    Find the way back

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -26,4 +26,8 @@ export class HeroDetailComponent implements OnInit {
     this.heroService.getHero(id).subscribe(h => this.hero = h);
   }
 
+  goBack(): void {
+    this.location.back();
+  }
+
 }
