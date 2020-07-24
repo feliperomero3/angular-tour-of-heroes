dbe247a69e5b7bf6e0f59b7b594ee1ef37dd3fbf
Felipe Romero <feliperomero3@outlook.com>
2020-07-23 23:30:46 -0500

    Remove prefix rule from tslint

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -33,7 +33,7 @@ export class HeroDetailComponent implements OnInit {
   save(): void {
     this.heroService.updateHero(this.hero).subscribe({
       next: () => this.goBack()
-    })
+    });
   }
 
 }
