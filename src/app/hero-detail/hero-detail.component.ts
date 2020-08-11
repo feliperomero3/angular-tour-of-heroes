cde5677f250ab0e33aab294060893d3c4fb77999
Felipe Romero <feliperomero3@outlook.com>
2020-08-11 00:21:17 -0500

    Remove unused import of 'Input'

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Location } from '@angular/common';
 import { ActivatedRoute } from '@angular/router';
 import { Hero } from '../hero';
